5566c56dd795b7508c658d4f2d1075364815beb0
Code Tiger <cd.tgr9@gmail.com>
2022-11-01 20:34:01 +0100

    build fix

--- src/entry.esm.ts
+++ src/entry.esm.ts
@@ -1,21 +1,23 @@
-import { App, DefineComponent, Plugin } from 'vue';
+import { App, DefineComponent, Plugin } from "vue";
 
 // Import vue component
-import component from '@/component/index.vue';
+import component from "@/component/Index.vue";
 
 // Define typescript interfaces for installable component
-type InstallableComponent = DefineComponent<{}, {}, any> & { install: Exclude<Plugin['install'], undefined> };
+type InstallableComponent = DefineComponent<{}, {}, any> & {
+  install: Exclude<Plugin["install"], undefined>;
+};
 
 // Default export is installable instance of component.
 // IIFE injects install function into component, allowing component
 // to be registered via Vue.use() as well as Vue.component(),
-export default /*#__PURE__*/((): InstallableComponent => {
+export default /*#__PURE__*/ ((): InstallableComponent => {
   // Assign InstallableComponent type
   const installable = component as unknown as InstallableComponent;
 
   // Attach install function executed by Vue.use()
   installable.install = (app: App) => {
-    app.component('Vue3QTelInput', installable);
+    app.component("Vue3QTelInput", installable);
   };
   return installable;
 })();
